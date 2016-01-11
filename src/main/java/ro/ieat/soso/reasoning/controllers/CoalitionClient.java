846b426437ae5b481753d14edbe588eff4ef331a
Adrian Spataru <adi.spataru92@gmail.com>
2016-01-11 11:04:23 +0200

    CoalitionClient can send jobRequests.

--- src/main/java/ro/ieat/soso/reasoning/controllers/CoalitionClient.java
+++ src/main/java/ro/ieat/soso/reasoning/controllers/CoalitionClient.java
@@ -2,6 +2,7 @@ package ro.ieat.soso.reasoning.controllers;
 
 import org.springframework.web.client.RestTemplate;
 import ro.ieat.soso.core.coalitions.Coalition;
+import ro.ieat.soso.core.jobs.Job;
 
 /**
  * Created by adrian on 10.01.2016.
@@ -11,11 +12,17 @@ import ro.ieat.soso.core.coalitions.Coalition;
 public class CoalitionClient {
 
     private static RestTemplate restTemplate;
-    private static String targetUrl;
+    private static String coalitionTargetUrl;
+    private static String jobRequestTargetUrl;
 
     public static void sendCoalition(Coalition c){
         restTemplate = new RestTemplate();
-        restTemplate.put(targetUrl, c);
+        restTemplate.put(coalitionTargetUrl, c);
 
+    }
+
+    public static void sendJobRequest(Job j){
+        restTemplate = new RestTemplate();
+        restTemplate.postForEntity(jobRequestTargetUrl, j, Object.class);
     }
 }
