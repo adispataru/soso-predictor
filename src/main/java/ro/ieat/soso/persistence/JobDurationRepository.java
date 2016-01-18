236d246ddd886f70f5066647f4691823cc9b9b00
Adrian Spataru <adi.spataru92@gmail.com>
2016-01-18 12:26:35 +0200

    Job duration repository.

---
+++ src/main/java/ro/ieat/soso/persistence/JobDurationRepository.java
@@ -0,0 +1,18 @@
+package ro.ieat.soso.persistence;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import ro.ieat.soso.predictor.prediction.JobDuration;
+
+import java.util.List;
+
+/**
+ * Created by adrian on 10.01.2016.
+ * For maintaining job duration predictions...
+ */
+public interface JobDurationRepository extends MongoRepository<JobDuration, Long>{
+
+    List<JobDuration> findByLogicJobName(String logicJobName);
+    List<JobDuration> findBySubmitTimeBetween(Long min, Long max);
+    List<JobDuration> findByLogicJobNameAndSubmitTimeGreaterThan(String logicJobName, Long max);
+
+}
