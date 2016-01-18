2f6a92de5230009bacad1adf12eded1122313d75
Adrian Spataru <adi.spataru92@gmail.com>
2016-01-18 20:54:03 +0200

    Added feature: can add task usage, divide to obtain average.

--- src/main/java/ro/ieat/soso/evaluator/UsageError.java
+++ src/main/java/ro/ieat/soso/evaluator/UsageError.java
@@ -4,6 +4,7 @@ import ro.ieat.soso.core.jobs.TaskUsage;
 
 /**
  * Created by adrian on 18.01.2016.
+ * This class is used by FineTuner to compute average usage prediction error
  */
 public class UsageError {
 
@@ -21,6 +22,37 @@ public class UsageError {
         this.maxCpu = predicted.getMaxCpu() - actual.getMaxCpu();
         this.maxMemory = predicted.getMaxMemory() - actual.getMaxMemory();
         this.maxDisk = predicted.getMaxDisk() - actual.getMaxDisk();
+    }
+
+    private UsageError(){
+        this.cpu = .0;
+        this.memory = .0;
+        this.disk = .0;
+        this.maxCpu = .0;
+        this.maxMemory = .0;
+        this.maxDisk = .0;
+    }
+
+    public static UsageError getZeroUsageError(){
+        return new UsageError();
+    }
+
+    public void addError(UsageError error){
+        this.cpu += error.getCpu();
+        this.memory += error.getMemory();
+        this.disk += error.getDisk();
+        this.maxCpu += error.getMaxCpu();
+        this.maxMemory += error.getMaxMemory();
+        this.maxDisk += error.getMaxDisk();
+    }
+
+    public void divide(int l){
+        this.cpu /= l;
+        this.memory /= l;
+        this.disk /= l;
+        this.maxCpu /= l;
+        this.maxMemory /= l;
+        this.maxDisk /= l;
     }
 
     public double getCpu() {
@@ -69,5 +101,9 @@ public class UsageError {
 
     public void setMaxDisk(double maxDisk) {
         this.maxDisk = maxDisk;
+    }
+
+    public String toStringforPlot(){
+        return "" + cpu + " " + maxCpu + " " + memory + " " + maxMemory + " " + disk  + " " + maxDisk;
     }
 }
