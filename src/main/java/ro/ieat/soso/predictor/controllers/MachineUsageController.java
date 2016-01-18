ba0756a6a6ba2b08cb0496683737f9ddc88f9ba1
Adrian Spataru <adi.spataru92@gmail.com>
2016-01-18 06:12:15 +0200

    MachineUsageController assigns machineId to taskUsage based on scheduled job

--- src/main/java/ro/ieat/soso/predictor/controllers/MachineUsageController.java
+++ src/main/java/ro/ieat/soso/predictor/controllers/MachineUsageController.java
@@ -5,12 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ro.ieat.soso.core.coalitions.Coalition;
-import ro.ieat.soso.core.coalitions.Machine;
 import ro.ieat.soso.core.jobs.Job;
 import ro.ieat.soso.core.jobs.ScheduledJob;
 import ro.ieat.soso.core.jobs.TaskUsage;
-import ro.ieat.soso.evaluator.TestTaskUsage;
-import ro.ieat.soso.persistence.*;
+import ro.ieat.soso.persistence.CoalitionRepository;
+import ro.ieat.soso.persistence.JobRepository;
+import ro.ieat.soso.persistence.MachineRepository;
+import ro.ieat.soso.persistence.TaskUsageMappingRepository;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -29,27 +30,16 @@ public class MachineUsageController {
     TaskUsageMappingRepository taskUsageMappingRepository;
     @Autowired
     CoalitionRepository coalitionRepository;
-    @Autowired
-    TestTaskRepository testTaskRepository;
 
-    @RequestMapping(method = RequestMethod.POST, path = "/coalitions/{id}/schedule", consumes = "application/json")
+    @RequestMapping(method = RequestMethod.PUT, path = "/coalitions/{id}/schedule", consumes = "application/json")
     public ResponseEntity<ScheduledJob> assignTaskUsageToMachine(@PathVariable long id, @RequestBody ScheduledJob job, HttpServletRequest request){
 
         Coalition coalition = coalitionRepository.findOne(id);
         List<TaskUsage> taskUsageList = taskUsageMappingRepository.findByJobId(job.getJobId());
-        long testid = testTaskRepository.count();
-        for(TaskUsage taskUsage : taskUsageList){
-            TestTaskUsage t = new TestTaskUsage();
-            t.endTime = taskUsage.getFinishTime();
-            t.startTime = taskUsage.getStartTime();
-            t.jobId = job.getJobId();
-            t.logicJobname = taskUsage.getLogicJobName();
-            t.machineId = job.getTaskMachineMapping().get(taskUsage.getTaskIndex());
-            t.coalitionId = id;
-            t.taskUsageId = taskUsage.getId();
-            testTaskRepository.save(t);
-        }
 
+        for(TaskUsage taskUsage : taskUsageList){
+            taskUsage.setAssignedMachineId(job.getTaskMachineMapping().get(taskUsage.getTaskIndex()));
+        }
 
         long jobId = job.getJobId();
         Job j = jobRepository.findOne(jobId);
@@ -62,71 +52,5 @@ public class MachineUsageController {
         }
 
     }
-
-//    @RequestMapping(method = RequestMethod.POST, path = "assign/usage")
-//    public ResponseEntity<String> assignRestOfJobs(){
-//
-//
-//        for(Job j : machineRepository.jobRepo.values()){
-//            if(machineRepository.assignedJobs.contains(j.getJobId()))
-//                continue;
-//
-//            for(TaskHistory taskHistory : j.getTaskHistory().values()){
-//                long taskId = taskHistory.getTaskIndex();
-//                long machineId = taskHistory.getMachineId();
-//                Machine m = machineRepository.findOne(machineId);
-//                if(m != null)
-//                    m.getTaskUsageList().add(taskHistory.getTaskUsage());
-//                else{
-//                    System.out.printf("Machine not found!: %d", machineId);
-//                }
-//            }
-//            machineRepository.assignedJobs.add(j.getJobId());
-//
-//        }
-//
-//
-//        return new ResponseEntity<String>("ok", HttpStatus.OK);
-//
-//
-//    }
-
-    @RequestMapping(method =  RequestMethod.POST, path = "/assign/normalJob/{id}")
-    public ResponseEntity<String> assignJob(@PathVariable("id") long id){
-
-        Job j = jobRepository.findOne(id);
-
-//        for(TaskHistory taskHistory : j.getTaskHistory().values()){
-//            long taskId = taskHistory.getTaskIndex();
-//            long machineId = taskHistory.getMachineId();
-//            Machine m = machineRepository.findOne(machineId);
-//            if(m != null) {
-//                m.getTaskUsageList().add(taskHistory.getTaskUsage());
-//                machineRepository.save(m);
-//            }
-//            else{
-//                System.out.printf("Machine not found!: %d", machineId);
-//            }
-//        }
-
-
-        return new ResponseEntity<String>("ok", HttpStatus.OK);
-    }
-
-    @RequestMapping(method =  RequestMethod.POST, path = "/assign/usage/{id}/{usageId}", consumes = "application/json")
-    public ResponseEntity<Long> assignUsage(@PathVariable("id") long id, @PathVariable("usageId") Long usage){
-
-            Machine m = machineRepository.findOne(id);
-            if(m != null) {
-                m.getTaskUsageList().add(usage);
-                machineRepository.save(m);
-            }
-            else{
-                System.out.printf("Machine not found!: %d", id);
-            }
-
-        return new ResponseEntity<Long>(usage, HttpStatus.OK);
-    }
-
 
 }
