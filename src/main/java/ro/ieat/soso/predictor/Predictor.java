3e7a7ae596430ca3532ea34ecf53845c5ee95cda
Adrian Spataru <adi.spataru92@gmail.com>
2016-01-18 05:22:04 +0200

    Predictor implemented without new scheduled jobs.

--- src/main/java/ro/ieat/soso/predictor/Predictor.java
+++ src/main/java/ro/ieat/soso/predictor/Predictor.java
@@ -4,12 +4,9 @@ package ro.ieat.soso.predictor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ro.ieat.soso.core.coalitions.Machine;
-import ro.ieat.soso.core.coalitions.Usage;
-import ro.ieat.soso.core.config.Configuration;
 import ro.ieat.soso.core.jobs.Job;
 import ro.ieat.soso.core.jobs.TaskUsage;
-import ro.ieat.soso.core.prediction.DurationPrediction;
-import ro.ieat.soso.core.prediction.MachinePrediction;
+import ro.ieat.soso.core.prediction.Duration;
 import ro.ieat.soso.persistence.JobRepository;
 import ro.ieat.soso.persistence.MachineRepository;
 import ro.ieat.soso.persistence.TaskUsageMappingRepository;
@@ -17,8 +14,9 @@ import ro.ieat.soso.predictor.prediction.PredictionFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  * Created by adrian on 11.12.2015.
@@ -39,27 +37,43 @@ public  class Predictor {
         List<TaskUsage> taskUsageList = taskUsageMappingRepository.
                 findByStartTimeGreaterThanAndFinishTimeLessThan(historyStart, historyEnd);
         for(Machine m : machineRepository.findAll()){
-            List<Usage> usageList = new ArrayList<Usage>();
-            long time = System.currentTimeMillis();
-            for(TaskUsage taskUsage : taskUsageList){
-                //Get list before to avoid repeated interogation
-//            TaskUsage taskUsage = taskUsageMappingRepository.findOne(taskUsageId);
-                if(taskUsage.getMachineId() != m.getId())
-                    continue;
+            List<TaskUsage> usageList = taskUsageList.stream().filter(t -> t.getMachineId().equals(m.getId())).collect(
+                    Collectors.toList()
+            );
+            List<TaskUsage> queue = new LinkedList<>();
+            int processed = 1;
+            List<TaskUsage> predictedTaskUsageList = new ArrayList<>();
+            int lastIndex = 1;
+            TaskUsage lastTaskUsage = taskUsageList.get(0);
+            queue.add(lastTaskUsage);
+            while(processed < taskUsageList.size()){
+                TaskUsage taskUsage = taskUsageList.get(lastIndex);
+                if(taskUsage.getJobId() == lastTaskUsage.getJobId() &&
+                        taskUsage.getTaskIndex() == lastTaskUsage.getTaskIndex()){
+                    queue.add(taskUsage);
 
-                for (Usage u : taskUsage.getUsageList()) {
-                    if (u.getStartTime() < historyStart)
-                        continue;
-                    if (u.getEndTime() > historyEnd)
-                      break;
-                    usageList.add(u);
+                }else{
+                    if(!queue.isEmpty()){
+                        TaskUsage predicted = (TaskUsage) PredictionFactory.getPredictionMethod("taskUsage").predict(queue);
+                        predictedTaskUsageList.add(predicted);
+                        queue.clear();
+                    }
+                    queue.add(taskUsage);
                 }
-
+                processed++;
+                lastIndex++;
             }
-            MachinePrediction prediction = PredictionFactory.predictMachineUsage(usageList);
-            prediction.setStartTime(historyEnd * Configuration.TIME_DIVISOR);
-            prediction.setEndTime((historyEnd + Configuration.STEP) * Configuration.TIME_DIVISOR);
-            m.setPrediction(prediction);
+            TaskUsage machineUsage = new TaskUsage();
+            for(TaskUsage tu : predictedTaskUsageList){
+                if(machineUsage.getEndTime() < tu.getEndTime())
+                    machineUsage.setEndTime(tu.getEndTime());
+                if(machineUsage.getStartTime() < tu.getStartTime())
+                    machineUsage.setStartTime(tu.getStartTime());
+                machineUsage.addTaskUsage(tu);
+            }
+
+
+            m.setUsagePrediction(machineUsage);
             machineRepository.save(m);
         }
 
@@ -68,60 +82,70 @@ public  class Predictor {
 
 
     @RequestMapping(method = RequestMethod.POST, path = "/predict/usage/{historyStart}/{historyEnd}", consumes = "application/json")
-    public MachinePrediction predictMachineUsage(@RequestBody Machine m, @PathVariable long historyStart,
+    public TaskUsage predictMachineUsage(@RequestBody Machine m, @PathVariable long historyStart,
                                                  @PathVariable long historyEnd) throws IOException, InterruptedException {
 
-//        String path = Configuration.MACHINE_USAGE_PATH + "/" + machineId;
-//        Machine m = MachineUsageMapper.readOne(new File(path), historyStart, historyEnd);
-        List<Long> machineUsage =  m.getTaskUsageList();
-//        Logger.getLogger("Predictor").info("tasks: " + machineUsage.size());
 
-        List<Usage> usageList = new ArrayList<Usage>();
         List<TaskUsage> taskUsageList = taskUsageMappingRepository.
-                findByMachineIdAndStartTimeLessThan(m.getId(), historyEnd);
-        Logger.getLogger("Predictor").info("Task usage list size " + taskUsageList.size());
-        for(TaskUsage taskUsage : taskUsageList){
-            //Get list before to avoid repeated interogation
-//            TaskUsage taskUsage = taskUsageMappingRepository.findOne(taskUsageId);
+                findByStartTimeGreaterThanAndFinishTimeLessThan(historyStart, historyEnd);
+        List<TaskUsage> usageList = taskUsageList.stream().filter(t -> t.getMachineId().equals(m.getId())).collect(
+                Collectors.toList()
+        );
+        List<TaskUsage> queue = new LinkedList<>();
+        int processed = 1;
+        List<TaskUsage> predictedTaskUsageList = new ArrayList<>();
+        int lastIndex = 1;
+        TaskUsage lastTaskUsage = taskUsageList.get(0);
+        queue.add(lastTaskUsage);
+        while(processed < taskUsageList.size()){
+            TaskUsage taskUsage = taskUsageList.get(lastIndex);
+            if(taskUsage.getJobId() == lastTaskUsage.getJobId() &&
+                    taskUsage.getTaskIndex() == lastTaskUsage.getTaskIndex()){
+                queue.add(taskUsage);
 
-            for (Usage u : taskUsage.getUsageList()) {
-                if (u.getStartTime() < historyStart)
-                    continue;
-//                irrelevant
-//                if (u.getEndTime() > historyEnd)
-//                    continue;
-                usageList.add(u);
+            }else{
+                if(!queue.isEmpty()){
+                    TaskUsage predicted = (TaskUsage) PredictionFactory.getPredictionMethod("taskUsage").predict(queue);
+                    predictedTaskUsageList.add(predicted);
+                    queue.clear();
+                }
+                queue.add(taskUsage);
             }
-
+            processed++;
+            lastIndex++;
+        }
+        TaskUsage machineUsage = new TaskUsage();
+        for(TaskUsage tu : predictedTaskUsageList){
+            if(machineUsage.getEndTime() < tu.getEndTime())
+                machineUsage.setEndTime(tu.getEndTime());
+            if(machineUsage.getStartTime() < tu.getStartTime())
+                machineUsage.setStartTime(tu.getStartTime());
+            machineUsage.addTaskUsage(tu);
         }
 
 
-        MachinePrediction prediction = PredictionFactory.predictMachineUsage(usageList);
-        prediction.setStartTime(historyEnd * Configuration.TIME_DIVISOR);
-        prediction.setEndTime((historyEnd + Configuration.STEP) * Configuration.TIME_DIVISOR);
-        m.setPrediction(prediction);
+        m.setUsagePrediction(machineUsage);
+        machineRepository.save(m);
 
-        //Eventually this would become a call via REST
-        //MachineUsagePredictionController.updateMachineStatus(m.getId(), m)
-
-        return prediction;
+        return machineUsage;
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/predict/job/{logicJobName}/{historyEnd}")
-    public DurationPrediction predictJobRuntime(@PathVariable final String logicJobName, Long historyEnd) throws IOException {
+    public Duration predictJobRuntime(@PathVariable final String logicJobName, Long historyEnd) throws IOException {
 
 
+        //This should be reasoned as a similarity strategy
         List<Job> jobs = jobRepository.findByLogicJobNameAndSubmitTimeLessThan(logicJobName, historyEnd);
-        List<Long> durationList = new ArrayList<Long>();
+        List<Duration> durationList = new ArrayList<Duration>();
         for(Job j : jobs){
             if(j.getFinishTime() == 0 || j.getScheduleTime() == 0 || !"finish".equals(j.getStatus()))
                 continue;
-            Long duration = j.getFinishTime() - j.getScheduleTime();
-            if(duration > 0)
+            Duration duration = new Duration(j.getFinishTime() - j.getScheduleTime());
+            if(duration.longValue() > 0)
                 durationList.add(duration);
         }
 
-        DurationPrediction duration = PredictionFactory.predictTime(durationList);
+        Duration duration = (Duration) PredictionFactory.getPredictionMethod("job").predict(durationList);
 //        if(duration != null) {
 //            JobRuntimePredictionController.updateJobDuration(logicJobName, duration);
 //        }
