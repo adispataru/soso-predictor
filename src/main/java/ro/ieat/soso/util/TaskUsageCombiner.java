8bcd22e96e8b47cc0f73244349fee40b64fdef06
adispataru <adi.spataru92@gmail.com>
2016-02-16 17:54:35 +0200

    TaskCombiner refactoring

--- src/main/java/ro/ieat/soso/util/TaskUsageCombiner.java
+++ src/main/java/ro/ieat/soso/util/TaskUsageCombiner.java
@@ -13,7 +13,7 @@ import java.util.logging.Logger;
  * This class contains logic for combining tasks on a given resource.
  */
 public class TaskUsageCombiner {
-    public static int sampleSize = 30;
+    public static int sampleSize = 300;
 
     public static TaskUsage combineTaskUsageList(List<TaskUsage> usageList, long startTime,
                                                  List<Job> jobs, List<ScheduledJob> scheduledJobs, String type){
@@ -24,18 +24,18 @@ public class TaskUsageCombiner {
             total[i] = new TaskUsage();
         }
         for(TaskUsage taskUsage : usageList){
-            long sch = getTimeToStartByJobIdAndScheduleType(taskUsage.getJobId(), type, scheduledJobs);
-            long jobStart = getJobScheduleTime(jobs, taskUsage.getJobId());
+//            long sch = getTimeToStartByJobIdAndScheduleType(taskUsage.getJobId(), type, scheduledJobs);
+//            long jobStart = getJobScheduleTime(jobs, taskUsage.getJobId());
 
             // mindbogglingly offset computation 'cause I can!
-            long offset = (sch != 0 && jobStart != 0) ? (sch - jobStart) / Configuration.TIME_DIVISOR : 0;
-
-            if(Math.abs(offset) > sampleSize) {
-                Logger.getLogger("Combiner").info("job " + taskUsage.getJobId() + " has offset " + offset);
-                Logger.getLogger("Combiner").info("job start" + jobStart + "; scheduled start " + sch);
-                continue;
-            }
-//            long offset = 0;
+//            long offset = (sch != 0 && jobStart != 0) ? (sch - jobStart) / Configuration.TIME_DIVISOR : 0;
+//
+//            if(Math.abs(offset) > sampleSize) {
+//                Logger.getLogger("Combiner").info("job " + taskUsage.getJobId() + " has offset " + offset);
+//                Logger.getLogger("Combiner").info("job start" + jobStart + "; scheduled start " + sch);
+//                continue;
+//            }
+            long offset = 0;
             long thisTaskStart = taskUsage.getStartTime() / Configuration.TIME_DIVISOR - startTime;
             long thisTaskEnd = taskUsage.getEndTime() / Configuration.TIME_DIVISOR - startTime;
             for(long i = thisTaskStart + offset; i < thisTaskEnd + offset && i < sampleSize; i++){
