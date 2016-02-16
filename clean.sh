ff2dfbb664796cc101791f9ea6ab553dadae4b54
adispataru <adi.spataru92@gmail.com>
2016-02-16 12:27:38 +0200

    Ready for experiment

--- clean.sh
+++ clean.sh
@@ -1,4 +1,9 @@
-rm ./output/results/load/*
-rm ./output/results/runtime/*
-rm ./output/results/schedule/*
-rm ./output/results/usageError/*
+rm -r ./output/results/load/*
+rm -r ./output/results/runtime/*
+rm -r ./output/results/schedule/*
+rm -r ./output/results/usageError/*
+
+mkdir -p ./output/results/load
+mkdir -p ./output/results/runtime
+mkdir -p ./output/results/schedule
+mkdir -p ./output/results/usageError
