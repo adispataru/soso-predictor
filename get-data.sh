590a88ff519fa6678d85ef31d0770fe092960605
adispataru <adi.spataru92@gmail.com>
2016-03-05 02:17:57 +0200

    Added base directory

--- get-data.sh
+++ get-data.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-mkdir ./data
+base=pwd
+mkdir "$base/data"
 ## declare an array variable
 declare -a arr=("machine_events" "job_events" "task_events" "task_usage")
 
@@ -7,14 +8,14 @@ declare -a arr=("machine_events" "job_events" "task_events" "task_usage")
 url="gs://clusterdata-2011-2"
 for folder in "${arr[@]}"
 do
-   mkdir "./data/$folder"
+   mkdir "$base/data/$folder"
    for i in 0 1 2 3 4
    do
-        gsutil cp "$url/$folder/part-0000$i-of-00500.csv.gz" "./data/$folder/"
-        gsutil cp "$url/$folder/part-0000$i-of-00001.csv.gz" "./data/$folder/"
+        gsutil cp "$url/$folder/part-0000$i-of-00500.csv.gz" "$base/data/$folder/"
+        gsutil cp "$url/$folder/part-0000$i-of-00001.csv.gz" "$base/data/$folder/"
    done
-   cd "./data/$folder"
+   cd "$base/data/$folder"
    gzip -d *.gz
-   cd ../
+   cd $base
    # or do whatever with individual element of the array
 done
