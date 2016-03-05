b190be3b296e11621d410e8c01d3d40f2070c94d
adispataru <adi.spataru92@gmail.com>
2016-03-05 02:05:12 +0200

    Added data script

--- get-data.sh
+++ get-data.sh
@@ -4,9 +4,10 @@ mkdir ./data
 declare -a arr=("machine_events" "job_events" "task_events" "task_usage")
 
 ## now loop through the above array
-url="gs://clusterdata-2011-2/"
+url="gs://clusterdata-2011-2"
 for folder in "${arr[@]}"
 do
+   mkdir "./data/$folder"
    for i in 0 1 2 3 4
    do
         gsutil cp "$url/$folder/part-0000$i-of-00500.csv.gz" "./$folder/"
