e8761b32158bc58af328c72cd7b883b0c19bcb7c
Adrian Spataru <adi.spataru92@gmail.com>
2016-04-14 14:04:57 +0300

    Refactored clean script

--- clean.sh
+++ clean.sh
@@ -1,7 +1,4 @@
-rm -r ./output/results/load/*
-rm -r ./output/results/runtime/*
-rm -r ./output/results/schedule/*
-rm -r ./output/results/usageError/*
+rm -r ./output/results/*
 
 mkdir -p ./output/results/load
 mkdir -p ./output/results/runtime
